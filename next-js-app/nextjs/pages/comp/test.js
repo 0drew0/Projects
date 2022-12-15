ab406018c707e4da6d2d30c34ffc5f5990033662
0drew0 <drewm@bk.ru>
2022-12-15 16:55:44 +0300

    Add files via upload
    1st project using Next.js

---
+++ next-js-app/nextjs/pages/comp/test.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import styles from '../../styles/Home.module.css'
+import Link from 'next/link'
+
+const Test = ({ data }) => {
+  const [nextContent, setNextContent] = useState(5)
+  function goNext() {
+    if (nextContent >= 5) {
+      setNextContent((prev) => prev + 5)
+    }
+  }
+  function showBack() {
+    if (nextContent >= 5) {
+      setNextContent((prev) => prev - 5)
+    }
+  }
+  return (
+    <div className={styles.hell}>
+      <h1>CARDS :</h1>
+      {data.map((dat, index) => {
+        const context = createContext(dat)
+        return (
+          index < nextContent && (
+            <Link href={`${dat.id}`} key={dat.id}>
+              <div className={styles.card} key={dat.id}>
+                <p>id: {dat.id}</p>
+                <p>name: {dat.name}</p>
+                <p>email: {dat.email}</p>
+                <p>{dat.body}</p>
+                <context.Provider dat={dat} />
+              </div>
+            </Link>
+          )
+        )
+      })}
+      <br />
+      <button onClick={goNext} className={styles.butt}>
+        Show more
+      </button>
+      {nextContent > 5 && (
+        <button onClick={showBack} className={styles.butt}>
+          Show back
+        </button>
+      )}
+    </div>
+  )
+}
+export async function getServerSideProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/comments')
+  const data = await res.json()
+  return {
+    props: { data },
+  }
+}
+
+export default Test
