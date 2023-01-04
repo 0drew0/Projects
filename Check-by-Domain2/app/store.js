46b6fdff57d1b4ecc720a25ca7091bea37d3ffb7
0drew0 <drewm@bk.ru>
2023-01-04 17:29:56 +0300

    Delete Check-by-Domain2 directory

--- Check-by-Domain2/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import positionReducer from './Slices/positionSlice'
-
-export const store = configureStore({
-  reducer: {
-    pos: positionReducer,
-  },
-})
